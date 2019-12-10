5cd193bbc7d105b3eb8c27001890977e36e9b714
Samuel Kriikkula <samuel.kriikkula@gmail.com>
2019-12-10 13:25:11 +0000

    Revert "update"
    This reverts commit bb2c476a08ba112f8dbff9a6c5052a6391776e9c

--- examples/ex0.rs
+++ examples/ex0.rs
@@ -1,13 +1,47 @@
-use terminal_menu::{TerminalMenu, TerminalMenuItem};
+use terminal_menu::{
+    display,
+    TerminalMenuItem,
+    MultiSelectTerminalMenuItem,
+    NumericTerminalMenuItem
+};
 
 fn main() {
-    let mut tm = TerminalMenu::new(vec![
-        TerminalMenuItem::new("Test", vec!["First", "Second"]),
-        TerminalMenuItem::new("test2", vec!["Primary", "Secondary"]),
-        TerminalMenuItem::new("Exit", vec![])
-    ]);
 
-    tm.print();
-    tm.selected = 1;
-    tm.refresh();
+    let mut menu1: Vec<(String, Box<dyn TerminalMenuItem>)> = vec![
+        ("Multi".to_owned(), Box::new((MultiSelectTerminalMenuItem::new(vec![
+            "First".to_owned(),
+            "Second".to_owned(),
+            "Third".to_owned(),
+        ])))),
+        ("Numeric".to_owned(), Box::new(NumericTerminalMenuItem::new(
+            0.0,
+            -5.0,
+            0.5,
+            1.0))),
+        ("Submenu".to_owned(), Box::new(())),
+        ("Exit".to_owned(), Box::new(()))
+    ];
+    let mut menu2: Vec<(String, Box<dyn TerminalMenuItem>)> = vec![
+        ("AnotherMulti".to_owned(), Box::new(MultiSelectTerminalMenuItem::new(vec![
+            "First".to_owned(),
+            "Second".to_owned(),
+            "Third".to_owned(),
+        ]))),
+        ("AnotherNumeric".to_owned(), Box::new(NumericTerminalMenuItem::new(
+            0.0,
+            -5.0,
+            0.5,
+            1.0))),
+        ("Exit".to_owned(), Box::new(()))
+    ];
+
+    loop {
+        let result = display(&mut menu1, |_,_,_| {} ,true);
+        if result == 2 {
+            display(&mut menu2, |_,_,_| {}, true);
+        }
+        if result == 3 {
+            break;
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,76 +6,258 @@ use std::{
 };
 use crossterm::{
     execute,
-    terminal,
     cursor,
+    terminal,
+    input::{
+        input,
+        InputEvent,
+        KeyEvent,
+    },
+    screen::RawScreen,
 };
 
-pub struct TerminalMenuItem {
-    pub name: String,
-    pub values: Vec<String>,
-    pub selected: usize
+pub trait TerminalMenuItem {
+    fn left(&mut self);
+    fn right(&mut self); //____ to return or to not
+    fn enter(&mut self) -> bool;
+    fn print(&self);
 }
-impl TerminalMenuItem {
-    pub fn new(name: &str, values: Vec<&str>) -> TerminalMenuItem {
-        TerminalMenuItem {
-            name: name.to_owned(),
-            values: values.iter().map(|&s| s.to_owned()).collect(),
+
+impl TerminalMenuItem for () {
+    fn left(&mut self) { }
+    fn right(&mut self) { }
+    fn enter(&mut self) -> bool {
+        true
+    }
+    fn print(&self) { }
+}
+
+pub struct NumericTerminalMenuItem {
+    val: f64,
+    min: f64,
+    step: f64,
+    max: f64
+}
+impl NumericTerminalMenuItem {
+    pub fn new(default: f64, min: f64, step: f64, max: f64) -> NumericTerminalMenuItem {
+        NumericTerminalMenuItem {
+            val: default,
+            min,
+            step,
+            max
+        }
+    }
+}
+impl TerminalMenuItem for NumericTerminalMenuItem {
+    fn left(&mut self) {
+        self.val -= self.step;
+        if self.val < self.min {
+            self.val = self.min;
+        }
+    }
+    fn right(&mut self) {
+        self.val += self.step;
+        if self.val > self.max {
+            self.val = self.max;
+        }
+    }
+    fn enter(&mut self) -> bool {
+        false
+    }
+    fn print(&self) {
+        print!("{}", self.val);
+    }
+}
+
+pub struct MultiSelectTerminalMenuItem {
+    vals: Vec<String>,
+    selected: usize
+}
+impl MultiSelectTerminalMenuItem {
+    pub fn new(vals: Vec<String>) -> MultiSelectTerminalMenuItem {
+        if vals.len() == 0 {
+            panic!("'vals' cannot be empty");
+        }
+        MultiSelectTerminalMenuItem {
+            vals,
             selected: 0
         }
     }
 }
-
-pub struct TerminalMenu {
-    pub items: Vec<TerminalMenuItem>,
-    pub selected: usize
+impl TerminalMenuItem for MultiSelectTerminalMenuItem {
+    fn left(&mut self) {
+        if self.selected == 0 {
+            self.selected = self.vals.len() - 1;
+        }
+        else {
+            self.selected -= 1;
+        }
+    }
+    fn right(&mut self) {
+        if self.selected == self.vals.len() - 1 {
+            self.selected = 0;
+        }
+        else {
+            self.selected += 1;
+        }
+    }
+    fn enter(&mut self) -> bool {
+        self.right();
+        false
+    }
+    fn print(&self) {
+        print!("{}", self.vals[self.selected]);
+    }
 }
-impl TerminalMenu {
-    pub fn new(items: Vec<TerminalMenuItem>) -> TerminalMenu {
-        TerminalMenu {
-            items,
-            selected: 0
+
+fn move_up(a: u16) {
+    execute!(stdout(), cursor::MoveUp(a)).unwrap();
+}
+fn move_down(a: u16) {
+    execute!(stdout(), cursor::MoveDown(a)).unwrap();
+}
+fn move_to_x(x: u16) {
+    execute!(stdout(), cursor::MoveTo(x, cursor::position().unwrap().1)).unwrap();
+}
+
+fn update_value(
+    items: &mut Vec<(String, Box<dyn TerminalMenuItem>)>,
+    selected: usize,
+    longest_name: u16,
+    fun: fn(&str, &dyn TerminalMenuItem, usize) -> ()) {
+
+    move_up((items.len() - selected - 1) as u16);
+    move_to_x(longest_name + 7);
+    execute!(stdout(), terminal::Clear(terminal::ClearType::UntilNewLine)).unwrap();
+    items[selected].1.print();
+    move_down((items.len() - selected - 1) as u16);
+    move_to_x(0);
+    fun(&items[selected].0, items[selected].1.as_ref(), selected);
+}
+
+pub fn display(
+    mut items: &mut Vec<(String, Box<dyn TerminalMenuItem>)>,
+    fun: fn(&str, &dyn TerminalMenuItem, usize) -> (),
+    clear: bool) -> usize {
+
+    if items.len() == 0 {
+        panic!("'items' cannot be empty");
+    }
+
+    //into raw mode
+    let _raw = RawScreen::into_raw_mode();
+    execute!(stdout(), cursor::Hide).unwrap();
+
+    //get longest name length for alignment
+    let mut longest_name: u16 = 0;
+    for (name, _) in &*items {
+        if name.len() as u16 > longest_name {
+            longest_name = name.len() as u16;
         }
     }
-    pub fn print(&self) {
 
-        //calc longest name
-        let mut longest_name = 0;
-        for item in &self.items {
-            if item.name.len() > longest_name {
-                longest_name = item.name.len();
-            }
-        }
-
-        let mut i = 0;
-        for item in &self.items {
-            print!("{} {}",
-                if i == self.selected {'>'} else {' '},
-                item.name
-            );
-            if item.values.len() == 0 {
-                println!();
-            }
-            else {
-                move_to_x(longest_name as u16 + 6);
-                println!("{}", item.values[item.selected]);
-            }
-            i += 1;
-        }
-
+    //print initial stuff
+    print!("> {}", items[0].0);
+    move_to_x(longest_name + 7);
+    items[0].1.print();
+    for i in 1..items.len() {
+        println!();
+        move_to_x(2);
+        print!("{}", items[i].0);
+        move_to_x(longest_name + 7);
+        items[i].1.print();
     }
-    pub fn erase(&self) {
+    move_to_x(0);
+
+    //crossterm stuff
+    let input = input();
+    let mut sync_stdin = input.read_sync();
+
+    let mut selected: usize = 0;
+
+    loop {
+        if let Some(InputEvent::Keyboard(k)) = sync_stdin.next() {
+            match k {
+                KeyEvent::Up    | KeyEvent::Char('w') => {
+                    let mut to_move = items.len() - selected - 1;
+                    if to_move != 0 {
+                        move_up(to_move as u16);
+                    }
+                    print!(" ");
+                    move_to_x(0);
+                    if selected == 0 {
+                        selected = items.len() - 1;
+                        to_move = items.len() - 1;
+                        if to_move != 0 {
+                            move_down(to_move as u16);
+                        }
+                    }
+                    else {
+                        selected -= 1;
+                        move_up(1);
+                    }
+                    print!(">");
+                    move_to_x(0);
+                    to_move = items.len() - selected - 1;
+                    if to_move != 0 {
+                        move_down(to_move as u16);
+                    }
+                }
+                KeyEvent::Down  | KeyEvent::Char('s') => {
+                    let mut to_move = items.len() - selected - 1;
+                    if to_move != 0 {
+                        move_up(to_move as u16);
+                    }
+                    print!(" ");
+                    move_to_x(0);
+                    if selected == items.len() - 1 {
+                        selected = 0;
+                        to_move = items.len() - 1;
+                        if to_move != 0 {
+                            move_up(to_move as u16);
+                        }
+                    }
+                    else {
+                        selected += 1;
+                        move_down(1);
+                    }
+                    print!(">");
+                    move_to_x(0);
+                    to_move = items.len() - selected - 1;
+                    if to_move != 0 {
+                        move_down(to_move as u16);
+                    }
+                }
+
+                KeyEvent::Left  | KeyEvent::Char('a') => {
+                    items[selected].1.left();
+                    update_value(&mut items, selected, longest_name, fun);
+                },
+                KeyEvent::Right | KeyEvent::Char('d') => {
+                    items[selected].1.right();
+                    update_value(&mut items, selected, longest_name, fun);
+                },
+                KeyEvent::Enter => {
+                    if items[selected].1.enter() {
+                        break;
+                    }
+                    update_value(&mut items, selected, longest_name, fun);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    if clear {
         execute!(stdout(),
-            cursor::MoveUp(self.items.len() as u16),
+            cursor::MoveUp(items.len() as u16 - 1),
             terminal::Clear(terminal::ClearType::FromCursorDown)
         ).unwrap();
     }
-    pub fn refresh(&self) {
-        self.erase();
-        self.print();
+    else {
+        println!();
     }
-}
 
-//helper functions
-fn move_to_x(x: u16) {
-    execute!(stdout(), cursor::MoveTo(x, cursor::position().unwrap().1)).unwrap();
+    execute!(stdout(), cursor::Show).unwrap();
+    selected
 }
